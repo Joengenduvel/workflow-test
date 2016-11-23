645ff74d5938a45bd9a922dced6b1e4c64a05a2e
robbydecosemaeker <robbyd@infosupport.com>
2016-11-23 15:14:39 +0100

    Update run.R

--- R/run.R
+++ R/run.R
@@ -2,4 +2,4 @@ markupFile = "inst/demo.Rmd"
 
 cat('rendering ' , markupFile, "aka", tools::file_path_as_absolute(markupFile))
 
-rmarkdown::render(markupFile, output_file = "../output/page.html")
+rmarkdown::render(markupFile, output_file = "../output/index.html")
